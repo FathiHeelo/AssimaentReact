be8488dd32db30be0d72f60d52040d9344169319
FathiHeelo <s12323885@stu.najah.edu>
2025-03-20 18:56:47 +0300

    done

--- AssimaentReactpage/vite.config.js
+++ AssimaentReactpage/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root:"./index.html",
   plugins: [react()],build: {
     rollupOptions: {
       external: ['bootstrap']
