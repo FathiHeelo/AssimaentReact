fbbd5673f753c6d30841222bfdff60b0a43049fc
FathiHeelo <s12323885@stu.najah.edu>
2025-03-20 20:46:58 +0300

    done

--- AssimaentReactpage/src/App.jsx
+++ AssimaentReactpage/src/App.jsx
@@ -21,24 +21,24 @@ const App = () => {
   ]);
 
   const destinations = [
-    { image: "public/imges/ParisImage.png", name: "Paris", price: 699 },
-    { image: "public/imges/GreeceImage.png", name: "Greece", price: 1079 },
-    { image: "public/imges/NorwayImage.png", name: "Norway", price: 895 },
-    { image: "public/imges/TuscanyImage.png", name: "Tuscany", price: 1245 },
+    { image: "/public/imges/ParisImage.png", name: "Paris", price: 699 },
+    { image: "/public/imges/GreeceImage.png", name: "Greece", price: 1079 },
+    { image: "/public/imges/NorwayImage.png", name: "Norway", price: 895 },
+    { image: "/public/imges/TuscanyImage.png", name: "Tuscany", price: 1245 },
   ];
 
   const stays = [
-    { image: "public/imges/Discovery Shores Image.png", name: "Discovery Shores", price: 360, rating: 4.8 },
-    { image: "public/imges/Arctic Hut Image.png", name: "Arctic Hut", price: 420, rating: 4.7 },
-    { image: "public/imges/Lake Louise Image.png", name: "Lake Louise Inn", price: 244, rating: 4.6 },
-    { image: "public/imges/Matterhorn Suites Image.png", name: "Matterhorn Suites", price: 575, rating: 4.9 },
+    { image: "/public/imges/Discovery Shores Image.png", name: "Discovery Shores", price: 360, rating: 4.8 },
+    { image: "/public/imges/Arctic Hut Image.png", name: "Arctic Hut", price: 420, rating: 4.7 },
+    { image: "/public/imges/Lake Louise Image.png", name: "Lake Louise Inn", price: 244, rating: 4.6 },
+    { image: "/public/imges/Matterhorn Suites Image.png", name: "Matterhorn Suites", price: 575, rating: 4.9 },
   ];
 
   const holidays = [
-    { image: "public/imges/image_480.png", name: "Bali", duration: "4D3N", price: 899 },
-    { image: "public/imges/SwitzerlandImage.png", name: "Swiss", duration: "6D5N", price: 900 },
-    { image: "public/imges/BoracayImage.png", name: "Boracay", duration: "5D4N", price: 699 },
-    { image: "public/imges/PalawanImage.png", name: "Palawan", duration: "4D3N", price: 789 },
+    { image: "/public/imges/image_480.png", name: "Bali", duration: "4D3N", price: 899 },
+    { image: "/public/imges/SwitzerlandImage.png", name: "Swiss", duration: "6D5N", price: 900 },
+    { image: "/public/imges/BoracayImage.png", name: "Boracay", duration: "5D4N", price: 699 },
+    { image: "/public/imges/PalawanImage.png", name: "Palawan", duration: "4D3N", price: 789 },
   ];
 
   return (
